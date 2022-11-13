4f41ce5fc4a1f91bc9c3a54859e8b7b217efd2d3
MukundB2000 <79758028+Mukundb2000@users.noreply.github.com>
2022-11-13 14:47:32 +0530

    List files implementation
    List files implementation

--- file-store-ui/src/components/listfiles/index.js
+++ file-store-ui/src/components/listfiles/index.js
@@ -1,22 +1,123 @@
-import "../../App.css";
-import styled from "styled-components";
-import "./style.css";
-import { Routes, Route, useNavigate, BrowserRouter } from "react-router-dom";
-function ListFiles() {
-  const Button = styled.button`
-    background-color: #ffbb1c;
-    color: "#ffbb1c";
-    font-size: 20px;
-    padding: 10px 40px;
-    border-radius: 5px;
-    margin: 10px 0px;
-    cursor: pointer;
-  `;
-  return (
-    <div className="main">
-      <h2>UNDER DEVELOPMENT</h2>
-    </div>
-  );
-}
+// import React from 'react'
+// import styled from 'styled-components'
+// import { useTable } from 'react-table'
 
-export default ListFiles;
+// import makeData from './makeData'
+
+// const Styles = styled.div`
+//   padding: 1rem;
+
+//   table {
+//     border-spacing: 0;
+//     border: 1px solid black;
+
+//     tr {
+//       :last-child {
+//         td {
+//           border-bottom: 0;
+//         }
+//       }
+//     }
+
+//     th,
+//     td {
+//       margin: 0;
+//       padding: 0.5rem;
+//       border-bottom: 1px solid black;
+//       border-right: 1px solid black;
+
+//       :last-child {
+//         border-right: 0;
+//       }
+//     }
+//   }
+// `
+
+// function Table({ columns, data }) {
+//   // Use the state and functions returned from useTable to build your UI
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     rows,
+//     prepareRow,
+//   } = useTable({
+//     columns,
+//     data,
+//   })
+
+//   // Render the UI for your table
+//   return (
+//     <table {...getTableProps()}>
+//       <thead>
+//         {headerGroups.map(headerGroup => (
+//           <tr {...headerGroup.getHeaderGroupProps()}>
+//             {headerGroup.headers.map(column => (
+//               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+//             ))}
+//           </tr>
+//         ))}
+//       </thead>
+//       <tbody {...getTableBodyProps()}>
+//         {rows.map((row, i) => {
+//           prepareRow(row)
+//           return (
+//             <tr {...row.getRowProps()}>
+//               {row.cells.map(cell => {
+//                 return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+//               })}
+//             </tr>
+//           )
+//         })}
+//       </tbody>
+//     </table>
+//   )
+// }
+
+// function ListFiles() {
+//   const columns = React.useMemo(
+//     () => [
+//       {
+//         Header: 'File info',
+//         columns: [
+//           {
+//             Header: 'File name',
+//             accessor: 'firstName',
+//           },
+//           {
+//             Header: 'Total Size',
+//             accessor: 'lastName',
+//           },
+//         ],
+//       },
+//       {
+//         Header: 'Info',
+//         columns: [
+//           {
+//             Header: 'Uploaded Size',
+//             accessor: 'age',
+//           },
+//           {
+//             Header: 'Last Modified',
+//             accessor: 'visits',
+//           },
+//           {
+//             Header: 'Status',
+//             accessor: 'status',
+//           },
+//         ],
+//       },
+//     ],
+//     []
+//   )
+
+//   const data = React.useMemo(() => makeData(20), [])
+
+//   return (
+//     <Styles>
+//       <Table columns={columns} data={data} />
+//     </Styles>
+//   )
+// }
+
+// export default ListFiles
